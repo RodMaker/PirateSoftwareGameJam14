a31ae51139ff914b02f5455aaf3aa9d0b0c9aa71
RodMaker <83619630+RodMaker@users.noreply.github.com>
2024-01-12 22:47:48 +0000

    Initial Commit

---
+++ Assets/Scripts/Enemies/RandomIdleAnimation.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomIdleAnimation : MonoBehaviour
+{
+    private Animator myAnimator;
+
+    private void Awake()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!myAnimator)
+        {
+            return;
+        }
+
+        AnimatorStateInfo state = myAnimator.GetCurrentAnimatorStateInfo(0);
+        myAnimator.Play(state.fullPathHash, -1, Random.Range(0f, 1f));
+    }
+}
