a52b625302fc152c88c9b68453cc66aebeac6883
RodMaker <83619630+RodMaker@users.noreply.github.com>
2024-01-18 23:23:24 +0000

    BGM

---
+++ Assets/Scripts/BGM.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BGM : MonoBehaviour
+{
+    public static BGM Instance;
+
+    [SerializeField] AudioSource audioSource;
+
+    public AudioClip background1;
+    public AudioClip background2;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "Main Menu")
+        {
+            audioSource.Pause();
+        }
+
+        /* If I want to change songs
+        if (SceneManager.GetActiveScene().name == "Scene1")
+        {
+            audioSource.Pause();
+            audioSource.clip = background1;
+            audioSource.Play();
+        }
+
+        if (SceneManager.GetActiveScene().name == "Scene2")
+        {
+            audioSource.Pause();
+            audioSource.clip = background2;
+            audioSource.Play();
+        }
+        */
+    }
+}
